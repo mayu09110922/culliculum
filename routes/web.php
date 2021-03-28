1bf5cf69c7aadf0fe9f4db1d57b9abbb9af7a46f
mayu09110922 <akisaku03@gmail.com>
2021-03-28 21:12:27 +0900

    [add]ブログ投稿の作成

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostController@store');
