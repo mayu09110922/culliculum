4758d3cbc99f80caf3f3e8bb567cd920afbdc825
mayu09110922 <akisaku03@gmail.com>
2021-03-29 09:58:36 +0900

    [add]ブログ投稿の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -14,5 +14,25 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,15 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
+        <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
+        <h1>Blog Name</h1>
+        <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
