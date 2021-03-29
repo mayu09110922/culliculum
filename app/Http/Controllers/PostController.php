eedef966298d2e58da7a6755323eb7c80696a126
mayu09110922 <akisaku03@gmail.com>
2021-03-29 11:48:41 +0900

    [add]投稿削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,4 +25,19 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+   public function destroy(Post $post)
+   {
+    $post->delete();
+    return redirect('/');
+   }
 }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,9 +9,14 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
+        <h1>Blog Name</h1>
+        <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="from_delete" method="post">
+            {{ csrf_field() }}
+            {{ method_field('delete') }}
+            <input type="submit" style="display:none">
+            <p class='delete'>[<span onclick="return deletePost(this);">delete</span>]</p>
+        </form>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
@@ -23,5 +28,13 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e){
+                'use strict';
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
